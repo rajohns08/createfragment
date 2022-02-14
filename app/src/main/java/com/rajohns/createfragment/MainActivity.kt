f6fe11ab1c30dec4a59cd1b760c3c2c4821dfea2
Adam Johns <adam.johns@hyliion.com>
2022-02-14 15:59:37 -0600

    Fix compile issue

--- app/src/main/java/com/rajohns/createfragment/MainActivity.kt
+++ app/src/main/java/com/rajohns/createfragment/MainActivity.kt
@@ -3,6 +3,7 @@ package com.rajohns.createfragment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.commit
+import androidx.fragment.app.add
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
